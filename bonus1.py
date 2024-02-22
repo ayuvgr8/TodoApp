adb8eb0bd6f3da13e6e3682a0bb4639019970244
ayuvgr8 <ayuvgr8@gmail.com>
2024-02-22 20:58:34 +0530

    Initial Commit 1

---
+++ bonus1.py
@@ -0,0 +1,8 @@
+text = input("Enter a title:")
+# how many character does this string have ?
+length = len(text)
+print("The length of the title is ", length)
+
+
+# print(text.count("Asdsd"))
+
---
+++ todo.py
@@ -0,0 +1,13 @@
+user_prompt = "Enter the todo:"
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+# this is python List data type
+
+todos = [todo1 + 'abc', todo2, todo3, "Hello Ayush"]
+print(todos)
+
+print(type(user_prompt))
+# print(type(todos))
+# print(type(todo1))
