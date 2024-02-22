54f13af5da293db1075f75dc43ab19be7332bc6c
ayuvgr8 <ayuvgr8@gmail.com>
2024-02-22 21:05:43 +0530

    While loop

--- todo.py
+++ todo.py
@@ -1,13 +1,31 @@
 user_prompt = "Enter the todo:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-# this is python List data type
 
-todos = [todo1 + 'abc', todo2, todo3, "Hello Ayush"]
-print(todos)
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    print("Next...")
 
-print(type(user_prompt))
-# print(type(todos))
-# print(type(todo1))
+
+
+
+
+
+
+
+
+
+
+
+
+#  todo2 = input(user_prompt)
+#  todo3 = input(user_prompt)
+#
+# # this is python List data type
+#
+# todos = [todo1 + 'abc', todo2, todo3, "Hello Ayush"]
+# print(todos)
+#
+# print(type(user_prompt))
+# # print(type(todos))
+# # print(type(todo1))
