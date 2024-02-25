d2833983a660b9d1aed6e76271963afda569aaf4
ayuvgr8 <ayuvgr8@gmail.com>
2024-02-25 14:03:24 +0530

    Python- for loop /while loop (difference and list and string treated as sequences)

---
+++ Bonus/bonus3.py
@@ -0,0 +1,11 @@
+meals = ['cheese', 'pizza', 'pasta', 'salad']
+
+for meal in meals:
+    print(meal.capitalize())
+
+print("bye")
+
+# While loop run as long as condition is true
+# For loop runs as long as there are item in the list which is being iterated (can be iterated over string also)  for meal in 'meals':
+#
+# List and String are treated aa a sequences of Obj.
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ todos = []
 
 while True:
     user_action = input("Type Add, Show or Exit : ")
-    user_action = user_action.strip()
+    user_action = user_action.strip()  #strip() function is used to eliminate the extra spaces
 
     match user_action:
         case 'Add' | 'add':
@@ -12,6 +12,7 @@ while True:
             todos.append(todo)
         case 'Show' | 'Display' | 'show' | 'display':   #Bitwise Operator (Show or Display type anything  )
             for item in todos:
+                item = item.title()
                 print(item)
         case 'Exit' | 'exit':
             break
