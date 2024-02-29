6ba21fbe756f1f4c3a1261c98102610fa7bca032
ayuvgr8 <ayuvgr8@gmail.com>
2024-02-29 21:50:52 +0530

    file.readlines() and writelines()

--- main.py
+++ main.py
@@ -21,20 +21,30 @@ while True:
             file.close()
 
         case 'Show' | 'Display' | 'show' | 'display':  # bitwise Operator (Show or Display type anything  )
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):  # enumerate fn used here to get index
                 item = item.title()
                 row = f"{index + 1}.{item}"  # use of f"string" is here !
                 print(row)
+
         case 'edit' | 'Edit':  # we will use List Indexing Function
             number = int(input("Number of the todo to edit: "))  # we are converting str to int here
             number = number - 1  # indexing the todo
             new_todo = input("Enter the New Todo: ")
             todos[number] = new_todo  # access the items from the list and how to replace that item through that syntax
+
+
         case 'complete':
             number = int(input("Number of the todo to complete: "))
             todos.pop(number - 1)
+
+
         case 'Exit' | 'exit':
             break
+
         case _:
             print("Hey! You entered an invalid input(unknown command) please try again")
 
