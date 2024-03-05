40eae0449ca2890c3b20a54aeba261339983223b
ayuvgr8 <ayuvgr8@gmail.com>
2024-03-06 01:03:23 +0530

    file.readlines() and writelines()

--- Bonus/bonus6.py
+++ Bonus/bonus6.py
@@ -1,7 +1,10 @@
-contents = ["Hi,How you doin ?", "The files reported on DMS", "Presented the Pitch in front of the Sharks"]
+contents = ["Hi,How you doin ?"
+            "How you doin ?",
+            "The files reported on DMS.",
+            "Presented the Pitch in front of the Sharks."]
 
 filenames = ["docs.txt", "reports.txt", "presentation.txt"]
 
-for content, filename in enumerate(filenames):
+for content, filename in zip(contents, filenames):
     file = open(f"../files/{filename}", "w")
     file.write(content)
