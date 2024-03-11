37831559767c0a5c285bfbdecbe50f3cb1827555
ayuvgr8 <ayuvgr8@gmail.com>
2024-03-12 02:21:42 +0530

    Journal Bonus

---
+++ Bonus/bonus8.py
@@ -0,0 +1,3 @@
+date = input("Enter date : ")
+mood = input("How do  you date your today's mood  from 1 to 10 ?")
+journal = input("Let your thoughts flow:\n ")
---
+++ Experiments/e3.py
@@ -0,0 +1,7 @@
+with open("../files/docs.txt", "r")as file:
+    file.read()
+    content = file.read()
+    print(file.read())
+print(content)
+
+
