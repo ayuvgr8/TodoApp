c31d6029821ca59da8692edf798eaeb35463b5dd
ayuvgr8 <ayuvgr8@gmail.com>
2024-03-13 23:49:27 +0530

    Journal Bonus

--- Bonus/bonus8.py
+++ Bonus/bonus8.py
@@ -1,3 +1,7 @@
 date = input("Enter date : ")
 mood = input("How do  you date your today's mood  from 1 to 10 ?")
-journal = input("Let your thoughts flow:\n ")
+thoughts = input("Let your thoughts flow:\n ")
+
+with open(f"../journal/{date}.txt", "w") as file:
+    file.write(mood)
+    file.write(thoughts)
